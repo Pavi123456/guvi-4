d69ac4ee7dd52f1775a458980dc478f02a2a0570
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-07-27 12:03:23 +0530

    Create sumofnum.java

---
+++ sumofnum.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i;
+		Scanner s=new Scanner(System.in);
+		i=s.nextInt();
+		int count=0;
+		while(i !=0)
+		{
+			i /=10;
+			++count;
+		}
+		System.out.println(count);
+	}
+}
