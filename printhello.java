113bea77b941c3b93d773dc29aa3bbd97c4f30dc
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-07-27 11:28:19 +0530

    Update printhello.java

--- printhello.java
+++ printhello.java
@@ -14,7 +14,7 @@ class Ideone
 		Scanner s=new Scanner(System.in);
 		//System.out.print("N");
 		N=s.nextInt();
-		for(int i=0;i<N;i++)
+		for(int i=1;i<=N;i++)
 		{
 			System.out.println("Hello");
 		
