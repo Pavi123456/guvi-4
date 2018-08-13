92b6ffa47b1a5515fc64d0ce8f5b59cda035258b
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-08-13 22:06:05 +0530

    Create 90.java

---
+++ 90.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String st;
+		Scanner s=new Scanner(System.in);
+		st=s.nextLine();
+		for(int i=0;i<st.length();i++)
+		{
+			char ch=st.charAt(i);
+			if(Character.isDigit(ch))
+			{
+		      int n=Character.getNumericValue(ch);
+		      System.out.print(n);
+			}
+			
+		}
+	}
+}
