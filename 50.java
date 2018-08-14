2e6632468f09500abd183c4be6e4cdfefb6f7578
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-08-14 12:26:40 +0530

    Update 50.java

--- 50.java
+++ 50.java
@@ -14,20 +14,16 @@ class Ideone
 		boolean b=false;
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
-		if(n%2!=0)
-		{
-			System.out.println("no");
-			
-		}
-		else
-		{
-			for(int i=1;i<=n;i++)
+		for(int i=1;i<=n;i++)
 			{
 				if(Math.pow(2,i)==n)
 				b=true;
 			}
 			if(b==true)
 			System.out.println("yes");
+		else
+			System.out.println("no");
+	
 		}
 	}
 }
