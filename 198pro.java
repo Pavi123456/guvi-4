e419cdf0de26595f5bd9b8d1faecbd070205f5c0
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-09-17 16:27:32 +0530

    Create 198pro.java

---
+++ 198pro.java
@@ -0,0 +1,39 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner s=new Scanner(System.in);
+		
+		n=s.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		int max=a[0],min=a[0],temp=0,temp1=0;
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]>max){
+			max=a[i];
+			temp=i;
+			}
+			else
+			{
+			min=a[i];
+			temp1=i;
+			}
+	
+		}
+		int res=(temp-temp1);
+		System.out.print(res);
+	}
+}
