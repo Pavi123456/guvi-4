ff95244170b4ceb90066e39da25189bc224aea21
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-09-18 22:04:21 +0530

    Create 8hun.java

---
+++ 8hun.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				for(int k=j+1;k<n;k++)
+				{
+					if(a[i]+a[j]==a[k])
+					{
+						System.out.println(a[i]+" "+a[j]+" "+a[k]);
+					}
+				}
+			}
+		}
+	}
+}
