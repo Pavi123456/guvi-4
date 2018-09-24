b34185f72b795ab9adc5512efea0d19845052b0f
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-09-24 12:38:30 +0530

    Create 85pro.java

---
+++ 85pro.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes hereint res
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int res=~n;
+		System.out.println(res);
+		
+	}
+}
