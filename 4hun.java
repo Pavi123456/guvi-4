a190fd77230b0ec542446bcfdb9cfefdd881329c
tejayenuganti <40289649+tejayenuganti@users.noreply.github.com>
2018-11-25 00:23:09 +0530

    Create 4hun.java

---
+++ 4hun.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		char c;
+		int n=s.nextInt();
+		ArrayList<Character> a=new ArrayList<Character>();
+		for(int i=0;i<n;i++)
+		{
+			c=s.next().charAt(0);
+			a.add(c);
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(Collections.frequency(a,a.get(i))==1)
+			{
+				System.out.print(a.get(i));
+			}
+		}
+	}
+}
